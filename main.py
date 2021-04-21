7ac2a36bad7477459b0f76cdfe1eee3bb7575558
share424 <share424@gmail.com>
2021-04-21 21:22:44 +0700

    update

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-# from integration import findIntegration
-# from argparse import ArgumentParser
+from integration import findIntegration
+from argparse import ArgumentParser
 
-# if __name__ == "__main__":
-#     parser = ArgumentParser()
-#     parser.add_argument("url")
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("url")
 
-#     args = parser.parse_args()
+    args = parser.parse_args()
 
-#     integration = findIntegration(args.url)
-#     if integration is not None:
-#         integration.fetchAllSong()
-
-with open("meta.txt", "w") as file:
-    file.write("aaa")
+    integration = findIntegration(args.url)
+    if integration is not None:
+        integration.fetchAllSong()
